34c0c1bff8b4fc803b8dd76e1a8a180102be9f4e
Michal Wieczorek <wieczorekmichal1991@gmail.com>
2019-08-07 19:24:24 +0200

    First version of Calculator: - addition - substraction

---
+++ src/main/java/com/example/learning/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.example.learning.calculator;
+
+public interface Calculator {
+
+    int addition(int a, int b);
+    int substraction(int a, int b);
+
+}
---
+++ src/main/java/com/example/learning/calculator/CalculatorImpl.java
@@ -0,0 +1,13 @@
+package com.example.learning.calculator;
+
+public class CalculatorImpl implements Calculator {
+
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    public int substraction(int a, int b) {
+        return a - b;
+    }
+
+}
