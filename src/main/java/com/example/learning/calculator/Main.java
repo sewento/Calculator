e3d1f80f04c02a48b90e50d91b6c59070a55d714
Michal Wieczorek <wieczorekmichal1991@gmail.com>
2019-08-07 20:08:33 +0200

    Division by zero - fix

--- src/main/java/com/example/learning/calculator/Main.java
+++ src/main/java/com/example/learning/calculator/Main.java
@@ -27,6 +27,9 @@ public class Main {
                 case 4:
                     divisionOperation(scanner, calculator);
                     break;
+                case 5:
+                    moduloOperation(scanner, calculator);
+                    break;
                 default:
                     exit = true;
             }
@@ -36,32 +39,45 @@ public class Main {
 
     private static void printInfo() {
         System.out.println("**************************************************************************");
-        System.out.println("Choose operation: 1(+), 2(-), 3(*), 4(:), other number to exit");
+        System.out.println("Choose operation: 1(+), 2(-), 3(*), 4(:),, 5(%), other number to exit");
         System.out.println("Enter: ");
     }
 
     private static void additionOperation(Scanner scanner, Calculator calculator) {
         int[] twoNumbersFromInput = getTwoNumbersFromInput(scanner);
         int result = calculator.addition(twoNumbersFromInput[0], twoNumbersFromInput[1]);
-        System.out.println(twoNumbersFromInput[0] + " + " + twoNumbersFromInput[1] + " = " + result);
+        printResult(twoNumbersFromInput[0],twoNumbersFromInput[1],"+",result);
     }
 
     private static void substractOperation(Scanner scanner, Calculator calculator) {
         int[] twoNumbersFromInput = getTwoNumbersFromInput(scanner);
         int result = calculator.substraction(twoNumbersFromInput[0], twoNumbersFromInput[1]);
-        System.out.println(twoNumbersFromInput[0] + " - " + twoNumbersFromInput[1] + " = " + result);
+        printResult(twoNumbersFromInput[0],twoNumbersFromInput[1],"-",result);
     }
 
     private static void multiplicationOperation(Scanner scanner, Calculator calculator) {
         int[] twoNumbersFromInput = getTwoNumbersFromInput(scanner);
         int result = calculator.multiplication(twoNumbersFromInput[0], twoNumbersFromInput[1]);
-        System.out.println(twoNumbersFromInput[0] + " * " + twoNumbersFromInput[1] + " = " + result);
-    }
+        printResult(twoNumbersFromInput[0],twoNumbersFromInput[1],"*",result);    }
 
     private static void divisionOperation(Scanner scanner, Calculator calculator) {
         int[] twoNumbersFromInput = getTwoNumbersFromInput(scanner);
-        int result = calculator.division(twoNumbersFromInput[0], twoNumbersFromInput[1]);
-        System.out.println(twoNumbersFromInput[0] + " : " + twoNumbersFromInput[1] + " = " + result);
+        if(twoNumbersFromInput[1] == 0){
+            System.out.println("Nie można dzielić przez 0. ");
+        }else {
+            int result = calculator.division(twoNumbersFromInput[0], twoNumbersFromInput[1]);
+            System.out.println(twoNumbersFromInput[0] + " / "+ twoNumbersFromInput[1] + " = " + result);
+        }
+    }
+
+    private static void moduloOperation(Scanner scanner, Calculator calculator) {
+        int[] twoNumbersFromInput = getTwoNumbersFromInput(scanner);
+        int result = calculator.modulo(twoNumbersFromInput[0], twoNumbersFromInput[1]);
+        printResult(twoNumbersFromInput[0],twoNumbersFromInput[1],"%",result);
+    }
+
+    private static void printResult(int firstNumber, int secondNumber, String operator, int result) {
+        System.out.println(firstNumber + " " + operator + " " + secondNumber + " = " + result);
     }
 
     private static int[] getTwoNumbersFromInput(Scanner scanner) {
